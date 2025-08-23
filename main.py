e584bd92078a050a054c7f5d910684bcab5c4a86
Daniil669 <daniil.freeman669@gmail.com>
2025-08-23 12:32:43 +0300

    api call, output and verbose

--- main.py
+++ main.py
@@ -1,5 +1,50 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
 def main():
-    print("Hello from cli-ai-agent!")
+    
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+
+    if args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+
+    load_dotenv()
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    prompt_text = ' '.join(args)
+    messages = [types.Content(parts=[types.Part(text=prompt_text)], role="user")]
+
+    if verbose:
+         print(f"User prompt: {prompt_text}")
+
+    generate_response(client, messages, verbose)
+    
+
+def generate_response(client, messages, verbose):
+    
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
+
+    if verbose:
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+
+    print("Response:")
+    print(response.text)
 
 
 if __name__ == "__main__":
